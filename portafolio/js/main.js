1633af1c2b0d7806b5213dfc7720a6e9c34c1bdb
ayoub khalfaoui <eyub.kh@gmail.com>
2021-04-13 23:42:22 -0700

    Portafolio update

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -1,22 +1,22 @@
 const Cursor = document.getElementById('cursor');
-const Cursor2 = document.getElementById('cursor2');
 
-window.addEventListener('mousemove', pos => { 
-    Cursor.style.left = pos.clientX + 'px'; 
-    Cursor.style.top = pos.clientY + 'px'; 
-    Cursor2.style.left = pos.clientX + 'px'; 
-    Cursor2.style.top = pos.clientY + 'px'; 
-    if (pos.target.id !== 'cont1' ){
+window.addEventListener('mousemove', pos => {
+    Cursor.style.left = pos.clientX + 'px';
+    Cursor.style.top = pos.clientY + 'px';
+    Cursor2.style.left = pos.clientX + 'px';
+    Cursor2.style.top = pos.clientY + 'px';
+    if (!pos.target.classList.contains('img')) {
         Img1.style.visibility = 'hidden';
-        Img1.style.opacity = '0';    } 
-    if (pos.target.id !== 'cont2'){
+        Img1.style.opacity = '0';
+    }
+    if (!pos.target.classList.contains('img')) {
         Img2.style.visibility = 'hidden';
         Img2.style.opacity = '0';
-    } 
-    if (pos.target.id !== 'cont3'){
+    }
+    if (!pos.target.classList.contains('img')) {
         Img3.style.visibility = 'hidden';
         Img3.style.opacity = '0';
-    } 
+    }
 });
 
 
@@ -31,35 +31,43 @@ const Cont2 = document.getElementById('cont2');
 const Img3 = document.getElementById('img3');
 const Cont3 = document.getElementById('cont3');
 
-Cont1.addEventListener('mousemove' , pos => {
-    moveEffect(pos , Img1 , Cont1);
- });
-
-Cont2.addEventListener('mousemove' , pos => {
-    moveEffect(pos , Img2 , Cont2);
- });
- 
-Cont3.addEventListener('mousemove' , pos => {
-    moveEffect(pos , Img3 , Cont3);
+Cont1.addEventListener('mousemove', pos => {
+    moveEffect(pos, Img1, Cont1);
 });
 
- function moveEffect(pos , img , cont) {
-    img.style.left = pos.clientX + 'px';
-    img.style.top = pos.clientY + 'px';
+Cont2.addEventListener('mousemove', pos => {
+    moveEffect(pos, Img2, Cont2);
+});
+
+Cont3.addEventListener('mousemove', pos => {
+    moveEffect(pos, Img3, Cont3);
+});
+
+function moveEffect(pos, img, cont) {
+    let imgWidth = img.clientWidth / 2;
+    let imgHeight = img.clientHeight / 2;
+    let x = ((window.innerWidth / 2) - pos.clientX) / 10;
+    img.style.left = (pos.clientX - imgWidth) + 'px';
+    img.style.top = (pos.clientY - imgHeight) + 'px';
     img.style.visibility = 'visible';
     img.style.opacity = '1';
-    let move = cont.vanillaTilt.getValues()
-    console.log(move.tiltX)
-    img.style.transform = `rotateY(${move.tiltX }deg) rotateX(${move.tiltY }deg) translateY(-50%) translateX(-50%)`;
- };
+    img.style.transform = `rotate(${x}deg)`;
+    setTimeout(() => {
+        img.style.transform = `rotate(${x - x}deg)`;
+    }, 800)
+};
 
+// Cursor 2
 
-//  vanilla tilt test 
-VanillaTilt.init(Cont1);
-VanillaTilt.init(Cont2);
-VanillaTilt.init(Cont3);
+const Cursor2 = document.getElementById('cursor2');
 
-Cont1.addEventListener('tiltChange' , event => {
-    Img1.style.transform = `rotateY(${event.detail.tiltX }deg) rotateX(${event.detail.tiltY }deg) translateY(-50%) translateX(-50%)`;
-})
- 
+window.addEventListener('mousemove', pos => {
+    if(pos.target.classList.contains("hover")){
+        Cursor2.style.width = "50px"
+        Cursor2.style.height = "50px"
+    } else{
+        Cursor2.style.width = "0px"
+        Cursor2.style.height = "0px"
+    }
+});
+
